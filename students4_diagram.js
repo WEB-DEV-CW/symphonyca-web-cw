3ef1fa570850a93c34d16be393f8d7ba49d6d60d
sethubavan99 <sethubavan99@gmail.com>
2021-07-21 19:21:35 +0530

    commit

--- students4_diagram.js
+++ students4_diagram.js
@@ -1,19 +1,19 @@
 //link for presentation
-function function1() {location.href="#"};
+function function1() {location.href="index.html"};
 //link for home 
-function function2() {location.href="#"};
+function function2() {location.href="student2_mainpage.html"};
 //link for feedback 
-function function3() {location.href="#"};
+function function3() {location.href="student2_queryform.html"};
 //link for products 
-function function4() {location.href="#m"};
+function function4() {location.href="student1_products.html"};
 //link for sitemap 
 function function5() {location.href="students4_diagram.html"};
 //link for challenge 
-function function6() {location.href="#"};
+function function6() {location.href="student3_quiz.html"};
 //link for gallery 
 function function7() {location.href="students4_thumbnail.html"};
 //link for details 
-function function8() {location.href="#"};
+function function8() {location.href="details.html"};
 
 
 
